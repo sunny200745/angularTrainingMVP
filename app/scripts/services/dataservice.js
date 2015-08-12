1f4c2e1b0155b7df223d179186bced9fc06b8be6
sunny200745 <sunny.200745@gmail.com>
2015-08-12 16:47:34 +0530

    Added new service file

---
+++ app/scripts/services/dataservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angularTrainingMvpApp.dataService
+ * @description
+ * # dataService
+ * Service in the angularTrainingMvpApp.
+ */
+angular.module('angularTrainingMvpApp')
+  .service('dataService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
